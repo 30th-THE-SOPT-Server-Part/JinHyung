21052d58ba3fb9058041ea43ad3dd35730a3f666
Jin Hyung Park "Sigrid Jin <sigrid.jinhyung@gmail.com>
2022-04-29 11:35:16 +0900

    feat: BlogController to create article

---
+++ src/controllers/BlogController.ts
@@ -0,0 +1,31 @@
+import express, { Request, Response } from "express";
+import statusCode from "../modules/statusCode";
+import message from "../modules/responseMessage";
+import util from "../modules/util"
+
+
+import { BlogCreateRequestDto } from "../interfaces/blog/BlogCreateRequestDto";
+import { BlogService } from "../services";
+import { BlogUpdateRequestDto } from "../interfaces/blog/BlogUpdateRequestDto";
+
+const createBlog = async(request: Request, response: Response) => {
+    const blogCreateRequestDto: BlogCreateRequestDto = request.body;
+    
+    try {
+        const data = await BlogService.createBlog(blogCreateRequestDto);
+        response.status(statusCode.CREATED).send(
+            util.success(
+                statusCode.CREATED,
+                message.CREATED_BLOG_SUCCESS,
+                data
+            )
+        )
+    } catch (error) {
+        response.status(statusCode.INTERNAL_SERVER_ERROR).send(
+            util.fail(
+                statusCode.INTERNAL_SERVER_ERROR,
+                message.INTERNAL_SERVER_ERROR
+            )
+        )
+    }
+}
