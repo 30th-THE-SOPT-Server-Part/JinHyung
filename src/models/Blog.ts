63e8ad8e2dd72766995474490f08d41900491269
Jin Hyung Park "Sigrid Jin <sigrid.jinhyung@gmail.com>
2022-04-29 11:35:06 +0900

    feat: Blog models declared

---
+++ src/models/Blog.ts
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+import { UserInfo } from "../interfaces/user/UserInfo";
+import { UserSchema } from "./User";
+
+const BlogSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    contents: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    tags: [String],
+    commenters: [UserSchema]
+});
+
+export default mongoose.model<UserInfo & mongoose.Document>("User", UserSchema);
+export {UserSchema}
