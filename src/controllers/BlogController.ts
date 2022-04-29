c8d705d0666dd735fbccb42462c770d51e464b38
Jin Hyung Park "Sigrid Jin <sigrid.jinhyung@gmail.com>
2022-04-29 11:36:44 +0900

    feat: BlogController to updateBlog

--- src/controllers/BlogController.ts
+++ src/controllers/BlogController.ts
@@ -28,4 +28,21 @@ const createBlog = async(request: Request, response: Response) => {
             )
         )
     }
+
+    const updateBlog = async(request: Request, response: Response) => {
+        const blogUpdateDto: BlogUpdateRequestDto = request.body;
+        const { postId } = request.params;
+    
+        try {
+            await BlogService.updateBlog(postId, blogUpdateDto);
+    
+            response.status(statusCode.NO_CONTENT).send();
+        } catch (error) {
+            response.status(statusCode.INTERNAL_SERVER_ERROR).send(
+                util.fail(
+                    statusCode.INTERNAL_SERVER_ERROR, 
+                    message.INTERNAL_SERVER_ERROR
+            ));
+        }
+    }
 }
