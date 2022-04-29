22c3d692c3a98563aee41f11c6f7b4cfbce26305
Jin Hyung Park "Sigrid Jin <sigrid.jinhyung@gmail.com>
2022-04-29 11:42:02 +0900

    feat: BlogController to update and delete article

--- src/controllers/BlogController.ts
+++ src/controllers/BlogController.ts
@@ -5,7 +5,7 @@ import util from "../modules/util"
 
 
 import { BlogCreateRequestDto } from "../interfaces/blog/BlogCreateRequestDto";
-import { BlogService } from "../services";
+// import { BlogService } from "../services";
 import { BlogUpdateRequestDto } from "../interfaces/blog/BlogUpdateRequestDto";
 
 const createBlog = async(request: Request, response: Response) => {
@@ -16,7 +16,7 @@ const createBlog = async(request: Request, response: Response) => {
         response.status(statusCode.CREATED).send(
             util.success(
                 statusCode.CREATED,
-                message.CREATED_BLOG_SUCCESS,
+                message.CREATED_ARTICLE_SUCCESS,
                 data
             )
         )
@@ -28,21 +28,69 @@ const createBlog = async(request: Request, response: Response) => {
             )
         )
     }
+}
 
-    const updateBlog = async(request: Request, response: Response) => {
-        const blogUpdateDto: BlogUpdateRequestDto = request.body;
-        const { postId } = request.params;
-    
-        try {
-            await BlogService.updateBlog(postId, blogUpdateDto);
-    
-            response.status(statusCode.NO_CONTENT).send();
-        } catch (error) {
-            response.status(statusCode.INTERNAL_SERVER_ERROR).send(
-                util.fail(
-                    statusCode.INTERNAL_SERVER_ERROR, 
-                    message.INTERNAL_SERVER_ERROR
-            ));
-        }
+const updateBlog = async(request: Request, response: Response) => {
+    const blogUpdateDto: BlogUpdateRequestDto = request.body;
+    const { postId } = request.params;
+
+    try {
+        await BlogService.updateBlog(postId, blogUpdateDto);
+
+        response.status(statusCode.NO_CONTENT).send();
+    } catch (error) {
+        response.status(statusCode.INTERNAL_SERVER_ERROR).send(
+            util.fail(
+                statusCode.INTERNAL_SERVER_ERROR, 
+                message.INTERNAL_SERVER_ERROR
+        ));
     }
+}
+
+const findBlogById = async(request: Request, response: Response) => {
+    const { postId } = request.params;
+
+    try {
+        const data = await BlogService.findBlogById(postId); 
+
+        if (!data) {
+            return response.status(statusCode.NOT_FOUND).send(util.fail(statusCode.NOT_FOUND, message.NOT_FOUND));
+        }
+
+        response.status(statusCode.OK).send(
+            util.success(
+                statusCode.OK, 
+                message.READ_ARTICLE_SUCCESS, 
+                data
+        ));
+
+    } catch (error) {
+        response.status(statusCode.INTERNAL_SERVER_ERROR).send(
+            util.fail(
+                statusCode.INTERNAL_SERVER_ERROR, 
+                message.INTERNAL_SERVER_ERROR
+        ));
+    }
+}
+
+const deleteBlog = async(request: Request, response: Response) => {
+    const { postId } = request.params;
+
+    try {
+        await BlogService.deleteBlog(postId);
+
+        response.status(statusCode.NO_CONTENT).send();
+    } catch (error) {
+        response.status(statusCode.INTERNAL_SERVER_ERROR).send(
+            util.fail(statusCode.INTERNAL_SERVER_ERROR,
+                message.INTERNAL_SERVER_ERROR
+        ));
+    }
+}
+
+export default {
+    createBlog,
+    updateBlog,
+    findBlogById,
+    deleteBlog
 }
