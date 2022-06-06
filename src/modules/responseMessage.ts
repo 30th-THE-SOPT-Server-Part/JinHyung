55092c1e443a3d6817424bd52366d24192deaec3
Jin Hyung Park "Sigrid Jin <sigrid.jinhyung@gmail.com>
2022-06-06 13:04:32 +0900

    refactor: add jwtSecret and null token msgs

--- src/modules/responseMessage.ts
+++ src/modules/responseMessage.ts
@@ -2,7 +2,17 @@ const message = {
     NULL_VALUE: '필요한 값이 없습니다.',
     NOT_FOUND: '존재하지 않는 자원',
     BAD_REQUEST: '잘못된 요청',
-    INTERNAL_SERVER_ERROR: '서버 내부 오류'
+    
+    INTERNAL_SERVER_ERROR: '서버 내부 오류',
+    NULL_VALUE_TOKEN: '토큰이 없습니다.',
+    INVALID_TOKEN: '잘못된 토큰입니다',
+    DUPLICATED: '이메일 중복',
+    INVALID_PASSWORD: '잘못된 비밀번호입니다.',
+    SIGNIN_USER_SUCCESS: '성공',
+
+    CREATE_MOVIE_SUCCESS: "영화 생성 성공",
+    CREATE_COMMENT_SUCCESS: "영화 코멘트 생성 성공",
+    UPDATE_MOVIE_COMMENT_SUCCESS: "영화 리뷰 수정 성공",
 }
 
 export default message;
