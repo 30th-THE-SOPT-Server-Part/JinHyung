56394d4f7927158ee2b38776e2b9c49a50efa8bb
Jin Hyung Park "Sigrid Jin <sigrid.jinhyung@gmail.com>
2022-06-06 20:42:25 +0900

    feat: FileService to have a feature of upload file to s3

--- src/services/index.ts
+++ src/services/index.ts
@@ -2,5 +2,6 @@
 import UserService from "./UserService";
 import MovieService from "./MovieService";
 import ReviewService from "./ReviewService";
+import FileService from "./FileService";
 
-export { UserService, MovieService, ReviewService };
+export { UserService, MovieService, ReviewService, FileService };
